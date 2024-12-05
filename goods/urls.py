eac45402581ae81d67e62bacc752755fad71d8b4
Nikita <nikitazinovkin676@gmail.com>
2024-12-05 17:04:57 +0700

    the first pagination option

--- goods/urls.py
+++ goods/urls.py
@@ -5,6 +5,7 @@ app_name = 'goods'
 
 urlpatterns = [
     path('<slug:catalog_slug>/', views.catalog, name='index'),
+    path('<slug:catalog_slug>/<int:page>', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,16 +1,20 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
 
 from goods.models import Products
 
 
-def catalog(request, catalog_slug):
+def catalog(request, catalog_slug, page=1):
     goods = Products.objects.all()
     if catalog_slug != 'all':
         goods = get_list_or_404(goods.filter(category__slug=catalog_slug))
 
+    paginator = Paginator(goods,3)
+    current_page = paginator.page(page)
     context = {
         'title': 'Home - Каталог',
-        'goods': goods,
+        'goods': current_page,
+        'slug_url': catalog_slug,
     }
     return render(request, 'goods/catalog.html', context)
 
