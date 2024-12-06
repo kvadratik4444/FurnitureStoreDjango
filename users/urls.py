ed06076dbf4cd9acdc8a2b62b919612047db751e
Nikita <nikitazinovkin676@gmail.com>
2024-12-06 17:07:12 +0700

    Added templates for users app

---
+++ users/urls.py
@@ -0,0 +1,11 @@
+from users import views
+from django.urls import path
+
+app_name = 'users'
+
+urlpatterns = [
+    path('login/', views.login, name='login'),
+    path('registration/', views.registration, name='registration'),
+    path('profile/', views.profile, name='profile'),
+    path('logout/', views.logout, name='logout'),
+]
