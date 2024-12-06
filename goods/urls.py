24f8d9d7521a2769dd9849598f78d52aad0912d2
Nikita <nikitazinovkin676@gmail.com>
2024-12-06 08:05:56 +0700

    simple search created

--- goods/urls.py
+++ goods/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 app_name = 'goods'
 
 urlpatterns = [
+    path('search/', views.catalog, name='search'),
     path('<slug:catalog_slug>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
 
--- goods/views.py
+++ goods/views.py
@@ -1,20 +1,25 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
-
+from goods.utils import q_search
 from goods.models import Products
 
 
-def catalog(request, catalog_slug):
+def catalog(request, catalog_slug=None):
 
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     goods = Products.objects.all()
+
     if catalog_slug != 'all':
         goods = goods.filter(category__slug=catalog_slug)
 
+    if query:
+        goods = q_search(query)
     print(goods)
+
     if on_sale:
         goods = goods.filter(discount__gt=0)
 
