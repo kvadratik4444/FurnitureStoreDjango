d3015d709879fdd8101270e91241a3ebe84bd603
Nikita <nikitazinovkin676@gmail.com>
2024-12-07 16:05:38 +0700

    Login form and controller are ready

--- goods/urls.py
+++ goods/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('search/', views.catalog, name='search'),
     path('<slug:catalog_slug>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product'),
-
 ]
---
+++ users/views.py
@@ -0,0 +1,47 @@
+from django.contrib import auth
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_list_or_404
+from django.urls import reverse
+
+from users.forms import UserLoginForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
+    context = {
+        'title': 'Home - Авторизация',
+        'form': form,
+    }
+    return render(request, 'users/login.html', context)
+
+
+def registration(request):
+    context = {
+        'title': 'Home - Регистрация',
+    }
+    return render(request, 'users/registration.html', context)
+
+
+def profile(request):
+    context = {
+        'title': 'Home - Кабинет',
+    }
+    return render(request, 'users/profile.html', context)
+
+
+def logout(request):
+    context = {
+        'title': 'Home - Выход',
+    }
+    return render(request, '', context)
