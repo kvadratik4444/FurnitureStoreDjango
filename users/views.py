d5395a87e82299fac01bed3c49d5d7a362668939
Nikita <nikitazinovkin676@gmail.com>
2024-12-08 09:29:15 +0700

    login required decorator - redirect to target page

--- users/views.py
+++ users/views.py
@@ -17,7 +17,12 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f'{username} successfully logged')
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+
                 return HttpResponseRedirect(reverse('main:index'))
+
     else:
         form = UserLoginForm()
 
