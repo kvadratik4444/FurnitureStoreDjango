013977a277d7ebd9e71cd11b72cc1667fbf820cc
Nikita <nikitazinovkin676@gmail.com>
2024-12-12 15:49:35 +0700

    Admin panel setup

--- baskets/admin.py
+++ baskets/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from baskets.models import Basket
 
-admin.site.register(Basket)
+
+class BasketTabAdmin(admin.TabularInline):
+    model = Basket
+    fields = ('product', 'quantity', 'created_timestamp')
+    search_fields = ('product', 'quantity', 'created_timestamp')
+    readonly_fields = ('created_timestamp',)
+    extra = 1
+
+
+@admin.register(Basket)
+class BasketAdmin(admin.ModelAdmin):
+    list_display = ('user_display', 'product', 'quantity', 'created_timestamp')
+    search_fields = ('user__username',)
+    list_filter = ('created_timestamp', 'product')
+
+    def user_display(self, obj):
+        if obj.user:
+            return str(obj.user)
+        return "Анонимный пользователь"
--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from users.models import User
+from baskets.admin import BasketTabAdmin
 
-admin.site.register(User)
+# admin.site.register(User)
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+
+    inlines = (BasketTabAdmin,)
