d55254e3ac19a3bd5ed2caeb9fd3889516ff3c48
Nikita <nikitazinovkin676@gmail.com>
2024-12-13 17:04:33 +0700

    Added custom validators

--- orders/forms.py
+++ orders/forms.py
@@ -1,4 +1,6 @@
+import re
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class CreateOrderForm(forms.Form):
@@ -8,3 +10,17 @@ class CreateOrderForm(forms.Form):
     requires_delivery = forms.ChoiceField(choices=[('0', 'False'), ('1', 'True')])
     delivery_address = forms.CharField(required=False)
     payment_on_get = forms.ChoiceField(choices=[('0', 'False'), ('1', 'True')])
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data.get('phone_number')
+
+        if not phone_number.isdigit():
+            raise ValidationError("Номер телефона должен содержать только цифры.")
+
+        if len(phone_number) != 10:
+            raise ValidationError("Номер телефона должен содержать 10 цифр.")
+
+        if not re.match(r'^9\d{9}$', phone_number):
+            raise ValidationError("Номер телефона должен начинаться с 9 и содержать 10 цифр.")
+
+        return phone_number
