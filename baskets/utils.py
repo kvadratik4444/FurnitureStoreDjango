f3306e2d177fe9fd8fafca80a9ee704de75097b5
Nikita <nikitazinovkin676@gmail.com>
2024-12-13 18:01:12 +0700

    fixed display baskets

--- baskets/utils.py
+++ baskets/utils.py
@@ -3,8 +3,8 @@ from baskets.models import Basket
 
 def get_user_baskets(request):
     if request.user.is_authenticated:
-        return Basket.objects.filter(user=request.user).select_related('product')
+        return Basket.objects.filter(user=request.user).select_related('product').order_by('created_timestamp')
 
     if not request.session.session_key:
         request.session.create()
-    return Basket.objects.filter(session_key=request.session.session_key).select_related('product')
+    return Basket.objects.filter(session_key=request.session.session_key).select_related('product').order_by('created_timestamp')
